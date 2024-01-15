574b6bc9ca4c1c8868c63f12b223a7f319b7b782
Kartik Labhshetwar <141413343+KartikLabhshetwar@users.noreply.github.com>
2024-01-15 22:34:57 +0530

    Add files via upload

---
+++ src/components/Home.jsx
@@ -0,0 +1,68 @@
+import homeProfile from "/src/assets/img/profile-pic.jpg";
+import curvedArrow from "/src/assets/img/curved-arrow.svg";
+import randomLines from "/src/assets/img/random-lines.svg";
+
+function Home() {
+  return (
+    <section className="home section" id="home">
+      <div className="home__container container grid">
+        <h1 className="home__name">Kartik Labhshetwar</h1>
+
+        <div className="home__profile">
+          <div className="home__image">
+            <img src={homeProfile} alt="image" className="home__img" />
+            <div className="home__shadow"></div>
+            <img src={curvedArrow} alt="" className="home__arrow" />
+            <img src={randomLines} alt="" className="home__line" />
+            <div className="geometric-box"></div>
+          </div>
+
+          <div className="home__social">
+            <a
+              href="https://www.linkedin.com/in/kartik017/"
+              target="_blank"
+              className="home__social-link"
+            >
+              <i className="ri-linkedin-box-line"></i>
+            </a>
+            <a
+              href="https://github.com/KartikLabhshetwar"
+              target="_blank"
+              className="home__social-link"
+            >
+              <i className="ri-github-line"></i>
+            </a>
+            <a
+              href="https://twitter.com/Kartiktwts"
+              target="_blank"
+              className="home__social-link"
+            >
+              <i className="ri-twitter-line"></i>
+            </a>
+            <a
+              href="https://www.instagram.com/kartiklabhshetwar017"
+              target="_blank"
+              className="home__social-link"
+            >
+              <i className="ri-instagram-line"></i>
+            </a>
+          </div>
+        </div>
+
+        <div className="home__info">
+          <p className="home__description">
+            <b>Full Stack Developer</b> with knowledge in Web Development and DSA, I offer the best projects resulting in quality work.
+          </p>
+          <a href="#about" className="home__scroll">
+            <div className="home__scroll-box">
+              <i className="ri-arrow-down-s-line"></i>
+            </div>
+            <span className="home__scroll-text">Scroll Down</span>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,73 @@
+import projects1 from "/src/assets/img/project-1.jpg";
+import projects2 from "/src/assets/img/project-2.jpg";
+import projects3 from "/src/assets/img/project-3.jpg";
+
+function Projects() {
+  const projects = [
+    {
+      subtitle: "Website",
+      title: "To-Do App",
+      description:
+        "Dominate your day with Task Master, a powerful ReactJS to-do list app designed to streamline your tasks and boost your productivity. Add new tasks effortlessly, edit them on the fly, and mark achievements with satisfaction. Task Master is more than just a checklist; it's a productivity powerhouse waiting to be unleashed.",
+      demo: "",
+      sourceCode: "https://github.com/KartikLabhshetwar/React-Notes/tree/main/10todocontextLocal",
+      projectImg: projects1,
+    },
+    {
+      subtitle: "Web app",
+      title: "Weather App",
+      description:
+        "Craving instant weather updates? My weather app delivers real-time conditions for any spot on Earth. Just type a location, and boom! Temperature, humidity, wind speed – all displayed beautifully. Responsive design? Check. Error handling? Covered. Future upgrades? You bet (multi-day forecasts, anyone?). Ditch the guesswork, embrace the weather!",
+      demo: "",
+      sourceCode: "https://github.com/KartikLabhshetwar/Weather-App-",
+      projectImg: projects2,
+    },
+    {
+      subtitle: "Web app",
+      title: "Music Player App",
+      description:
+        "This interactive web player, crafted with HTML, CSS, and JavaScript, lets you control your music with ease. Play, pause, and switch tracks seamlessly, all while enjoying a sleek and intuitive UI. It's a springboard for further features like volume control, playlists, and more. Dive in and let the music flow!",
+      demo: "https://music-player-app-neon-sigma.vercel.app/",
+      sourceCode: "https://github.com/KartikLabhshetwar/Music-Player-App",
+      projectImg: projects3,
+    }
+  ];
+
+  const projectElems = projects.map((project) => (
+    <article className="projects__card" key={project.demo}>
+      <div className="projects__image">
+        <img src={project.projectImg} alt="image" className="projects__img" />
+        <a
+          // href={project.demo}
+          // target="_blank"
+          className="projects__button button"
+        >
+          <i className="ri-arrow-right-up-line"></i>
+        </a>
+      </div>
+
+      <div className="projects__content">
+        <h3 className="projects__subtitle">{project.subtitle}</h3>
+        <h2 className="projects__title">{project.title}</h2>
+        <p className="projects__description">{project.description}</p>
+      </div>
+
+      <div className="projects__buttons">
+        <a href={project.sourceCode} target="_blank" className="projects__link">
+          <i className="ri-github-line"></i> View
+        </a>
+      </div>
+    </article>
+  ));
+
+  return (
+    <section className="projects section" id="projects">
+      <h2 className="section__title-1">
+        <span>Projects.</span>
+      </h2>
+      <div className="projects__container container grid">{projectElems}</div>
+    </section>
+  );
+}
+
+export default Projects;
